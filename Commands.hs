4fb7cad352600c0e6a7d4e428ce29c419d3fc32c
AlbusSerpents <naidim@abv.bg>
2017-12-29 18:03:31 +0200

    Commands module first part. Put the main data types

---
+++ Commands.hs
@@ -0,0 +1,27 @@
+module Commands
+(
+	Pwd(..),
+	Cd(..),
+	Ls(..),
+	Cat(..),
+	Rm(..),
+)
+
+where
+
+import System.FilePath
+import System.Process
+clear = system "clear"
+
+data Path = Full {content :: String} | Relative {content :: String} 
+	deriving Show
+
+data Pwd = Current deriving Show
+data Cd = Cd {arg :: Path} deriving Show
+data Ls = Other {argument :: Maybe Path} deriving Show
+data Cat = Concat {files :: Maybe [Path], output :: Maybe Path} deriving Show
+data Rm = Remove {args :: [Path]} deriving Show
+
+instance Read Path where
+read f@ ('/':rest) = Full f 
+read p = Relative p 
