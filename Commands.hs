9010416e05e9e6a08845eee4b8a1b5a2f8c1b9c8
AlbusSerpents <naidim@abv.bg>
2018-01-09 17:36:54 +0200

    Created class Command and split the commands into diferent data types

--- Commands.hs
+++ Commands.hs
@@ -1,37 +1,19 @@
 module Commands
 (
-	Command,
-	Path
-)
 
+)
 where
 
-import System.FilePath
-import System.Directory
+import Path
+import FileSystem
 
-import System.Process
-clear = system "clear"
+data Pwd = Current deriving Show
+data Cd = Change {changeArg :: Path} deriving Show
+data Ls = List {listArg :: Maybe Path } deriving Show
+data Cat = Concat { from :: Maybe [Path], to :: Maybe Path } deriving Show	
+data Rm = Remove {removeArgs :: [Path]} deriving Show
 
-data Path = Full {content :: String} | Relative {content :: String} 
-	deriving Show
-
-data Command = 
-	Current | 
-	Change {arg :: Path}| 
-	List {argument :: Maybe Path} | 
-	Concat {files :: [Path], output :: Maybe Path} |
-	Write {text :: String, output :: Maybe Path} |
-	Remove {args :: [Path]} deriving Show
-	
-data CommandHeaders = PWD | CD | LS |CAT | RM deriving (Show, Read)
-
-resolvePath :: String -> Path
-resolvePath f @ ('/':rest) = Full f
-resolvePath p = Relative p 
-
-
-main = do
-	interact func
-
-func "." = ""
-func s = s
+class Command c where
+	execute :: c -> FileSystem -> Either (FileSystem, String) FileSystem
+	prepare :: c -> String
+	prepare _ = ""
