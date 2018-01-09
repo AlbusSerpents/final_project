58b04fb4314f9dd61df8e0fbd68ea0dd66aa86a2
AlbusSerpents <naidim@abv.bg>
2018-01-09 16:46:46 +0200

    Initial implementation of the Path module

---
+++ Path.hs
@@ -0,0 +1,36 @@
+module Path
+(
+	Path,
+	Name,
+	isFull,
+	isRelative,
+	fromString
+)
+
+where
+
+import System.FilePath (splitPath)
+
+type Name = String
+
+data Path = Full {content :: [Name]} | Relative {content :: [Name]} 
+	deriving (Show, Eq)
+	
+root = "/"	
+	
+isFull :: Path -> Bool
+isFull (Full _) = True
+isFull _ = False
+
+isRelative :: Path -> Bool
+isRelative (Relative _) = True
+isRelative _ = False
+
+fromString :: FilePath -> Path
+fromString fp 
+	| head split == root = Full split
+	| otherwise = Relative split
+	where
+		split = splitPath fp
+
+	
