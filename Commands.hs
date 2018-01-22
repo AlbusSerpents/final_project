23ddad92a8f6490dffde18cd4b08f70de645119b
AlbusSerpents <naidim@abv.bg>
2018-01-22 16:28:22 +0200

    added instances for ls and cd. cat in progress

--- Commands.hs
+++ Commands.hs
@@ -6,12 +6,15 @@ where
 
 import Path
 import FileSystem
-import System.IO
+import System.IO 
+import Data.Maybe (isJust, fromJust, mapMaybe)
+import Data.List (intersperse)
 
 data Pwd = Current deriving Show
 data Cd = Change {changeArg :: Path} deriving Show
 data Ls = List {listArg :: Maybe Path } deriving Show
-data Cat = Concat { from :: Maybe [Path], to :: Maybe Path } deriving Show	
+data Cat = Concat { from :: Maybe [Path], to :: Maybe Path } 
+	| ConsoleInput { text :: String, to :: Maybe Path } deriving Show	
 data Rm = Remove {removeArgs :: [Path]} deriving Show
 
 class Command c where
@@ -19,5 +22,36 @@ class Command c where
 	prepare :: c -> IO c
 	prepare = return
 	
+processOutput :: Maybe String -> String
+processOutput input
+	| isJust input = fromJust input
+	| otherwise = opperationFailed
+
+opperationFailed = "The requested operation failed. Non- fatal error!"	
+	
+
 instance Command Pwd where
-	execute Current f = Left (f, show $ fullFileName f)
+	execute curr f = Left (f, show $ fullFileName f)
+	
+instance Command Cd where
+	execute (Change p) f
+		| isJust found = Right $ fromJust found
+		| otherwise = Left (f, opperationFailed)
+		where
+			found = find f p
+			
+instance Command Ls where
+	execute (List td) f
+		| isJust td = 
+			let 
+				fs = find f $ fromJust td
+				contents = getChildrenNames fs
+			in
+				Left (f, processOutput contents)
+		| otherwise = Left (f, processOutput $ getChildrenNames $ Just f) 
+		
+		
+getChildrenNames :: Maybe FileSystem -> Maybe String
+getChildrenNames f = concatChildren <$> f
+	where
+		concatChildren = concat . intersperse ", " . map name . children
