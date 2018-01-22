df11eb9c1950510de3137bcacd0824f5882e21ef
AlbusSerpents <naidim@abv.bg>
2018-01-22 22:28:49 +0200

    Added cat instance and reworked parrent directories for cd

--- Commands.hs
+++ Commands.hs
@@ -14,20 +14,34 @@ data Pwd = Current deriving Show
 data Cd = Change {changeArg :: Path} deriving Show
 data Ls = List {listArg :: Maybe Path } deriving Show
 data Cat = Concat { from :: Maybe [Path], to :: Maybe Path } 
-	| ConsoleInput { text :: String, to :: Maybe Path } deriving Show	
+	| ConsoleInput { input :: String, to :: Maybe Path } deriving Show	
 data Rm = Remove {removeArgs :: [Path]} deriving Show
 
+type Response = Either (FileSystem, String) FileSystem
+
+isConcat :: Cat -> Bool
+isConcat (Concat _ _) = True
+isConcat _ = False
+
+isConsole :: Cat -> Bool
+isConsole (ConsoleInput _ _) = True
+isConsole _ = False
+
 class Command c where
-	execute :: c -> FileSystem -> Either (FileSystem, String) FileSystem
+	execute :: c -> FileSystem -> Response
 	prepare :: c -> IO c
 	prepare = return
 	
 processOutput :: Maybe String -> String
 processOutput input
 	| isJust input = fromJust input
-	| otherwise = opperationFailed
+	| otherwise = operationFailed
+	
+createResponse :: FileSystem -> Maybe FileSystem -> Response
+createResponse _ (Just success) = Right success
+createResponse baseContext Nothing = Left (baseContext, operationFailed)
 
-opperationFailed = "The requested operation failed. Non- fatal error!"	
+operationFailed = "The requested operation failed. Non- fatal error!"	
 	
 
 instance Command Pwd where
@@ -36,7 +50,7 @@ instance Command Pwd where
 instance Command Cd where
 	execute (Change p) f
 		| isJust found = Right $ fromJust found
-		| otherwise = Left (f, opperationFailed)
+		| otherwise = Left (f, operationFailed)
 		where
 			found = find f p
 			
@@ -55,3 +69,43 @@ getChildrenNames :: Maybe FileSystem -> Maybe String
 getChildrenNames f = concatChildren <$> f
 	where
 		concatChildren = concat . intersperse ", " . map name . children
+		
+instance Command Cat where
+	execute c fContext 
+		| isConcat c && hasSources && hasDestination = 
+			handleCatDestination fContext dest text
+		| isConcat c && hasSources && not hasDestination = Left (fContext, text)
+		| isConsole c && not hasDestination = Left (fContext, text)
+		| isConsole c && hasDestination = 
+			handleCatDestination fContext dest $ input c
+		| otherwise = Left (fContext, operationFailed)
+		where
+			hasSources = isJust $ from c
+			hasDestination = isJust $ to c
+			src = fromJust $ from c
+			dest = fromJust $ to c
+			text = getFileContents fContext src
+
+getFileContents :: FileSystem -> [Path] -> String	
+getFileContents f =  concat . map contents . filter isFile . mapMaybe (find f)
+
+handleCatDestination :: FileSystem -> Path -> String -> Response
+handleCatDestination fs dest text = 
+	createResponse fs $
+	Just fs >>= 
+	(\f -> writeToFile fs dest text) >>=
+	(\f -> find f $ fullFileName fs)
+
+writeToFile :: FileSystem -> Path -> String -> Maybe FileSystem
+writeToFile fs p text = 
+	let
+		newFile = find fs p
+		fileName = title p
+		parentPath = fromNames $ parents p
+	in
+		if isJust newFile then
+			write (fromJust newFile) text
+		else
+			Just fs >>= 
+			(\f -> find f parentPath) >>=
+			(\f -> file f fileName text)
