539302bb85c9126a0a5941a22bfe7cd54f70f09d
AlbusSerpents <naidim@abv.bg>
2018-01-22 12:34:54 +0200

    re-wrote some functions. Removed the dummy constructor value

--- FileSystem.hs
+++ FileSystem.hs
@@ -5,20 +5,20 @@ module FileSystem
 	file,
 	folder,
 	write,
-	append
+	append,
+	remove
 )
 
 where
 
 import Path hiding (fromString)
 import Data.Maybe (fromJust, isNothing, isJust, listToMaybe)
-import Data.List (delete)
+import Data.List (deleteBy)
 import Data.Function (on)
 
 
 data FileSystem = File {name :: Name, contents :: String, parent :: Maybe FileSystem} 
 	| Folder {name :: Name, childern :: [FileSystem], parent :: Maybe FileSystem} 
-	| Dummy {name :: Name}
 
 instance Eq FileSystem where
 		a == b = on (==) name a b
@@ -49,6 +49,8 @@ file f n c
 		in 
 			link f new
 	| otherwise = Nothing
+	where
+		createFile f n c =  File n c $ Just f
 
 folder :: FileSystem -> Name -> Maybe FileSystem
 folder f n 
@@ -57,38 +59,29 @@ folder f n
 		in
 			link f new
 	| otherwise = Nothing
+	where
+		createFolder f n = Folder n [] $ Just f	
 
 link :: FileSystem -> FileSystem -> Maybe FileSystem
-link f new  = 	
-	if isJust $ addChild f new then
-		Just new
-	else
-		Nothing
-	
-addChild :: FileSystem -> FileSystem -> Maybe FileSystem
-addChild (File _ _ _) _ = Nothing
-addChild (Folder n ch p) c = Just $ Folder n (c:ch) p
-	
-createFile :: FileSystem -> Name -> String -> FileSystem
-createFile f n c =  File n c $ Just f
-	
-createFolder ::FileSystem -> Name -> FileSystem	
-createFolder f n = Folder n [] $ Just f	
+link f new  = addChild f new
+	where
+		addChild (File _ _ _) _ = Nothing
+		addChild (Folder n ch p) c = Just $ Folder n (c:ch) p
 	
 remove :: FileSystem -> Maybe FileSystem
 remove f = fmap removal $ parent f
 	where
-		removalName = Dummy $ name f
-		deleted = delete removalName . childern
-		removal e = Folder (name e) (deleted e) (parent e)
+		newChildren = deleteBy (on (==) name) f . childern
+		removal e = Folder (name e) (newChildren e) (parent e)
 	
 write :: FileSystem -> String -> Maybe FileSystem
 write (File name _  parent) text = Just $ File name text parent
 write _ _ = Nothing
 	
 append :: FileSystem -> String -> Maybe FileSystem
-append (File name curr parent) text = Just $ File name (curr ++ text) parent
-append _ _ = Nothing	
+append f text 
+	|isFile f = write f $  contents f ++ text
+	| otherwise = Nothing
 	
 find :: FileSystem -> Path -> Maybe FileSystem
 find fs p
@@ -96,11 +89,11 @@ find fs p
 	| isFull p = matching (root fs) $ content p 
 	| otherwise = Nothing
 	
-matching :: FileSystem -> [String] -> Maybe FileSystem
+matching :: FileSystem -> [Name] -> Maybe FileSystem
 matching fs (x:xs)
-	| xs == [] && fs == dummy = Just fs
-	| isFolder fs && fs == dummy = 
+	| xs == [] && nameEquality = Just fs
+	| isFolder fs && nameEquality = 
 		listToMaybe $ filter (isJust . flip matching xs) $ childern fs 
 	| otherwise = Nothing
 	where
-		dummy = Dummy x
+		nameEquality = name fs == x
