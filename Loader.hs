ec48dee1d1b4e11e8d1d1d713be4d1fffb206be4
AlbusSerpents <naidim@abv.bg>
2018-01-29 01:50:40 +0200

    intermediate state, monadFold still doesn't work

--- Loader.hs
+++ Loader.hs
@@ -15,7 +15,10 @@ import System.Directory
 selfRefering :: [String]
 selfRefering = [Path.this, Path.parentOnly]
 
-data ElememtType = File | Folder
+data ElementType = File {filePath :: FilePath, context :: FilePath} | 
+	Folder {folderPath :: FilePath, context :: FilePath} deriving Show 
+type Contents = Either [FilePath] String
+type ElementContext = (ElementType, Contents)
 
 load :: FilePath -> IO (FileSystem Bool)
 load = undefined
@@ -23,42 +26,59 @@ load = undefined
 createObjects :: FileSystem Bool -> FilePath -> FileSystem Bool
 createObjects = undefined
 
-bindTypes :: FilePath -> IO ElememtType
-bindTypes path = do
-	dir <- doesDirectoryExist path
+bindTypes :: (FilePath, FilePath) -> IO ElementType
+bindTypes (path, context) = do
+	dir <- doesDirectoryExist $ joinPath [context, path]
 	if dir then
-		return Folder
+		return $ Folder path context
 	else 
-		return File
+		return $ File path context
 
-handleContents :: 
-	FileSystem Bool -> FilePath -> 
-	(Either [FilePath] String) -> IO (FileSystem Bool)
-handleContents r path (Left dirContents) = 
+-- monadFold still doesn't work --
+handleContents :: FileSystem Bool -> ElementContext -> IO (FileSystem Bool)
+handleContents r (element, (Left dirContents)) = 
 	monadFold 
 		(return afterFolder)
 		dirContents
-		(\b -> \p -> bindTypes p >>= (contents p)  >>= (handleContents b p) )
+		(\b -> \p -> bindTypes (p, context element) >>= contents >>= (handleContents b) )
 	where
-		folderPath = fromString rootPath $ dropFileName path
-		folderName = takeFileName path
-		afterFolder = folder r folderPath folderName
-handleContents r path (Right fileContents) = 
-	return $ file r filePath fileName fileContents
+		path = folderPath element
+		folderLocation = elementPath path
+		folderName = elementName path
+		afterFolder = folder r folderLocation folderName
+handleContents r (element, (Right fileContents)) = 
+	return $ file r fileLocation fileName fileContents
 	where	
-		filePath = fromString rootPath $ dropFileName path
-		fileName = takeFileName path
+		path = filePath element
+		fileLocation = elementPath path
+		fileName = elementName path
+	
+elementPath :: FilePath -> Path
+elementPath = fromString rootPath . dropFileName
+
+elementName :: FilePath -> Name
+elementName = takeFileName	
 	
 monadFold :: (Monad m) => m a -> [b] -> (a -> b -> m a) -> m a
 monadFold b [] _ = b
 monadFold b (x:xs) f = b >>= (\bv -> f bv x)
 		
-contents :: FilePath -> ElememtType -> IO (Either [FilePath] String)
-contents path File = 
-		readFile path >>=
-		(return . Right)
-contents path Folder = 
-		return path >>= 
+contents :: ElementType -> IO ElementContext
+contents f@(File path context) = 
+	(readFile $ toRealWorldPath f) >>= 
+	(return . pair f . Right)
+contents f@(Folder path context) = 
+		return fullPath >>= 
 		getDirectoryContents >>= 
-		(return . Left . map (\e -> joinPath [path, e]) . filter (not . flip elem selfRefering))
+			(return . pair f . Left . 
+			map (\e -> joinPath [path, e]) . 
+			filter (not . flip elem selfRefering))
+		where
+			fullPath = toRealWorldPath f
 
+toRealWorldPath :: ElementType -> FilePath
+toRealWorldPath (File fp ctx) = joinPath [ctx, fp]
+toRealWorldPath (Folder fp ctx) = joinPath [ctx, fp]
+			
+pair :: ElementType -> Contents -> ElementContext
+pair p c = (p, c)
